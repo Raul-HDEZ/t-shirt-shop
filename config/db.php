03a2519a7ce85a071da63723af6a2695477a5cb6
raul-hdez <raul.hdez.2001@outlook.es>
2022-02-06 21:04:07 +0100

    Bootstrap

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 class Database{
 	public static function connect(){
-		//$db = new mysqli($_SERVER["endpoint"], $_SERVER["user"], $_SERVER["password"], $_SERVER["db"]);
-		$db = new mysqli('localhost', 'raul', 'raul', 'camisetas_master');
+		$db = new mysqli($_SERVER["endpoint"], $_SERVER["user"], $_SERVER["password"], $_SERVER["db"]);
+		//$db = new mysqli('localhost', 'raul', 'raul', 'camisetas_master');
 		//$db = new mysqli('localhost', 'root', 'root', 'tienda_master');
 		$db->query("SET NAMES 'utf8'");
 		return $db;
--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,6 @@
 <?php
-//define("base_url", $_SERVER["url"]);
-define("base_url", "http://localhost/julia/Tienda/");
+define("base_url", $_SERVER["url"]);
+//define("base_url", "http://localhost/julia/Tienda/");
 //define("base_url", "http://localhost/alumnoa/master-php/");
 define("controller_default", "productoController");
 define("action_default", "index");
